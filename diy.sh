f2847a5948b7bdacbb7d28cf7a00547512442c48
LoveyCat <lyh9420@126.com>
2020-02-21 10:18:35 +0800

    update script

--- diy.sh
+++ diy.sh
@@ -3,3 +3,6 @@ sed -i 's/UTC/CST-8/g' package/base-files/files/bin/config_generate
 sed -i '/CST-8/a \\t\tset system\.\@system\[\-1\]\.zonename=\'Asia\/Shanghai\' package/base-files/files/bin/config_generate
 sed -i "/lan/s/eth0/eth1/g" package/base-files/files/etc/board.d/99-default_network99-default_network
 sed -i "/wan/s/eth1/eth0/g" package/base-files/files/etc/board.d/99-default_network99-default_network
+sed -i '558s/4/8/g' target/linux/ar71xx/image/tiny-tp-link.mk
+sed -i 's/LUCI_DEPENDS:=/LUCI_DEPENDS:=+block-mount +kmod-usb-storage +kmod-usb-storage-extras +kmod-fs-ext4 +kmod-fs-exfat/g' feeds/luci/themes/luci-theme-bootstrap/Makefile
+sed -i 's/shadowsocks-libev-ss-redir/shadowsocks-libev/g' package/custom/luci-app-ssr-plus/Makefile
